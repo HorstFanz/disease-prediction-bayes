ae785696f44c2bd50eddd2a6ebe85196b31d75eb
Horst Franz <f_xaver_w@hotmail.com>
2025-06-08 16:59:12 +0200

    finished comments on code_SSVS.R and did multicolliniarity check in 03_model_SSVS.R

--- scripts/03_model_SSVS.R
+++ scripts/03_model_SSVS.R
@@ -1,4 +1,5 @@
 ## ----- 0.  House-keeping -------------------------------------------------
+# we simply took those from the original script 
 nsave <- 1000
 nburn <- 1000
 ntot  <- nsave + nburn        # total Gibbs draws
@@ -8,7 +9,7 @@ tau1 <- 10
 s0   <- 0.01                  # IG(a,b) prior on σ²
 S0   <- 0.01
 
-## ----- 1.  Read and build Y, X ------------------------------------------
+## ---- read and build Y, X ----
 dat <- read.csv("data/processed/final_table_avg.csv")
 
 Y <- as.matrix(dat$Estimate.y)       # response: malaria mortality (38 × 1)
@@ -23,8 +24,31 @@ X <- cbind(1,                                   # intercept
            scale(dat$Estimated.number.of.deaths.from.all.forms.of.tuberculosis),
            scale(dat$Share.of.population.residing.in.urban.areas..HYDE.estimates.and.UN.projections.))
 
+# scale normalizes all the predictor variables so they have mean 0 and sd 1.
+# important: if we want to interpret the results for the prediction later, we need to descale again
+
+# rename columns
 colnames(X) <- c("Intercept","GDP","Health","Pop65","MalariaInc",
                  "Density","TB_Mort","TB_Deaths","Urban")
 
-N <- nrow(X)            # 38
-K <- ncol(X)            # 9
+## ---- check for multicoliniarity of the predictors ----
+
+cor_matrix <- cor(X[,-1])  # Exclude the intercept
+print(round(cor_matrix, 2))  # Rounded for readability
+
+# nothing over 0.8 looks good
+
+
+library(car)
+
+# Fit a basic linear model (response can be arbitrary here, e.g., malaria deaths)
+y_dummy <- dat$Estimated.number.of.malaria.deaths
+lm_model <- lm(y_dummy ~ ., data = as.data.frame(X[,-1]))  # Exclude intercept manually
+vif(lm_model) # nothing over 5 looks good
+
+## ---- build actual model ----
+
+
+N <- nrow(X)            # number of countries
+K <- ncol(X)            # number of predictors
+
